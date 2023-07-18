1aaeb2c6f00b4f326967da29e07b5329ef3b5c98
Sukanya <75981356+sukanya-aa@users.noreply.github.com>
2023-07-18 13:49:52 +0530

    Update 445_add_two_numbers_II.py
    added comments

--- 445_add_two_numbers_II.py
+++ 445_add_two_numbers_II.py
@@ -33,10 +33,16 @@ Constraints:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+'''
+Using stacks to solve the problem. Technically, this is also a form of reversing the list.
+Stacks have Last In First Out (LIFO)
+'''
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         stack1 = []
         stack2 = []
+        #Append values of the input list to the stacks
         while l1:
             stack1.append(l1.val)
             l1 = l1.next
@@ -44,15 +50,23 @@ class Solution:
             stack2.append(l2.val)
             l2 = l2.next
 
-        c = 0
-        t_head = None
+        c = 0    #Carry   
+        t_head = None #Temporary head of resultant linked list
         while stack1 or stack2 or c:
             val1 = stack1.pop() if stack1 else 0
             val2 = stack2.pop() if stack2 else 0
 
             total = val1 + val2 + c
-            c = total // 10
-            new_n = ListNode(total % 10)
+            c = total // 10        #Floor operator will give the quotient rounded to the nearest integer(7//10=0)
+            new_n = ListNode(total % 10) #Modulo operator will return dividend if the divisor is greater than dividend(7 % 10 = 7) and the remainder, otherwise(13%10=3). This will be the value of this node(ex, 0 if total is 10)
+            '''
+            new_n.next = t_head: Link the new node to the temporary head.
+            The next attribute of new_n is assigned the current value of t_head.
+            This operation effectively links the new node to the current linked list under construction.
+            t_head = new_n: Update the temporary head to the new node.
+            t_head is updated to point to the newly created node new_n.
+            Since this node is now at the beginning of the linked list, it becomes the new head, and the previously created nodes are linked after it.
+            '''
             new_n.next = t_head
             t_head = new_n
         return t_head
