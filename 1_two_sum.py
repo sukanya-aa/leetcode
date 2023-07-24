ef38024fe8f5ab8d92a4af3af3d31c5c1f08567c
Sukanya <75981356+sukanya-aa@users.noreply.github.com>
2023-07-24 12:26:45 +0530

    Create 1_two_sum.py

---
+++ 1_two_sum.py
@@ -0,0 +1,53 @@
+'''
+1. Two Sum
+Tag: Easy
+https://leetcode.com/problems/two-sum/
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+
+ 
+Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Example 2:
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+Example 3:
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+Constraints:
+
+    2 <= nums.length <= 104
+    -109 <= nums[i] <= 109
+    -109 <= target <= 109
+    Only one valid answer exists.
+
+ 
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+'''
+
+class Solution:
+    #Comment/Uncomment whichever method you want to run
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #*******Nested Loop Method************#
+
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+                    break
+
+        #**********Hashtable Method*************#
+        num = {}
+        for i in range(len(nums)):
+            num[nums[i]] = i
+        for i in range(len(nums)):
+            c = target-nums[i]            #Take complement     
+            if c in num and num[c] != i:
+                return [num[c], i]
